9e207f419967c74e8b69f3ee1b889874cc4bcfcb
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-25 15:28:17 -0400

    Commit 2 - Lab 10 - Problems 1 & 2.

--- sdi-lab10/Fret_Kelly_lab10.js
+++ sdi-lab10/Fret_Kelly_lab10.js
@@ -10,15 +10,35 @@
 
 // Global variables
 var numFive = 5;
-var numThousand = 1000;
+var numThousand = 21;
 
 
 var fiveMultiples = function(numThou, num5) {
 	
+	incrFive = 0;
+	incrOne = 1;
+	incrSum = 0;
 	
+	while (incrOne < numThou) {
+		
+		incrFive = incrOne * num5;
+		incrOne++;
+		console.log("Increment by five = " + incrFive);
+		console.log("\n");
+		
+		incrSum = incrSum + incrFive;
+		//console.log("Increment sum = " + incrSum);
+		
+		//break;
+	};
 	
-	
+return incrSum;
 	
 };
 
-fiveMultiples(numThousand, num5);
+fiveMultiples(numThousand, numFive);
+
+console.log("Increment sum = " + incrSum);
+
+
+
