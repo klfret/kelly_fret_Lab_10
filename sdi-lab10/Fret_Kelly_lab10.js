d89c45bf12eb6ecdbb59c43e4be025f2c82344df
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-25 14:08:23 -0400

    Commit 1 - Lab 10 - Problems 1 & 2.

---
+++ sdi-lab10/Fret_Kelly_lab10.js
@@ -0,0 +1,24 @@
+//alert("Lab 10 - JavaScript works! - 25 September 2014");
+
+
+// Kelly L. Fret
+// SDI 1409
+// Lab 10
+// 25 September 2014
+
+
+
+// Global variables
+var numFive = 5;
+var numThousand = 1000;
+
+
+var fiveMultiples = function(numThou, num5) {
+	
+	
+	
+	
+	
+};
+
+fiveMultiples(numThousand, num5);
